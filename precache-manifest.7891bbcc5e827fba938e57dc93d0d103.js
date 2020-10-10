e8545e38968b3aef325dad89b336449dbd328681
ybZhang <472099249@qq.com>
2020-10-10 11:55:13 +0800

    deploy

---
+++ precache-manifest.7891bbcc5e827fba938e57dc93d0d103.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91404d66021185dc898e9272150351f",
+    "url": "/book-keeping-website/index.html"
+  },
+  {
+    "revision": "ff883b7b71d52858a1be",
+    "url": "/book-keeping-website/js/app.459d4128.js"
+  },
+  {
+    "revision": "9eff902950eeffa001fe",
+    "url": "/book-keeping-website/js/chunk-vendors.614f7a77.js"
+  },
+  {
+    "revision": "9687f909ab6db5f1b756e536556c3b17",
+    "url": "/book-keeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/book-keeping-website/robots.txt"
+  }
+]);
